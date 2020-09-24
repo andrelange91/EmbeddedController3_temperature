e46a746326cd55555202e0266a22b51e39e0ad8c
André Lange <andre@lait.dk>
2020-09-24 13:06:59 +0200

    Initial helpers

---
+++ app/Helpers/ApiHelper.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Helpers;
+
+class ApiHelper{
+
+    public function GetHighTemp(){
+        // call api and get the high temp for the day.
+    }
+    public function GetLowTemp(){
+        // call api and get the high temp for the day.
+    }
+    public function RegisterTemp(){
+        // call the api and update with a new temp reading
+    }
+    private function HttpRequest(){
+        // generic function to handle the http request.
+    }
+}
