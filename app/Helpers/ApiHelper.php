d41b5eb4642829eb054a0c5f88b28947e98395c6
André Lange <andre@lait.dk>
2020-10-05 10:05:57 +0200

    added curl post

--- app/Helpers/ApiHelper.php
+++ app/Helpers/ApiHelper.php
@@ -30,14 +30,43 @@ class ApiHelper{
     
     public function RegisterTemp($url, $data){
         // call the api and update with a new temp reading
-        $options = array(
-            'http' => array(
-                 'header'  => "Content-type: application/json",
-                'method'  => 'POST',
-                'content' => http_build_query($data)
-            )
-        );
-        $context  = stream_context_create($options);
-        return file_get_contents($url, false, $context);
+        // $options = array(
+        //     'http' => array(
+        //          'header'  => "Content-type: application/json",
+        //         'method'  => 'POST',
+        //         'content' => http_build_query($data)
+        //     )
+        // );
+        // $context  = stream_context_create($options);
+        // return file_get_contents($url, false, $context);
+
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, [
+        CURLOPT_PORT => "80",
+        CURLOPT_URL => $url,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => "",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 30,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "POST",
+        CURLOPT_POSTFIELDS => json_encode($data),
+        CURLOPT_HTTPHEADER => [
+            "content-type: application/json"
+        ],
+        ]);
+
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+
+        curl_close($curl);
+
+        if ($err) {
+        echo "cURL Error #:" . $err;
+        } else {
+        echo $response;
+        }
     }
 }
